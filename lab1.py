51408385dfe4c6c5815776732c9f7301cc47dd19
Arsenii <71705996+xpadx1@users.noreply.github.com>
2022-02-09 09:35:11 +0200

    Lab1.py file added

---
+++ lab1.py
@@ -0,0 +1,27 @@
+
+import sys
+import math
+
+a = int(input('Number a: '))
+b = int(input('Number b: '))
+c = int(input('Number c: '))
+
+if a == 0 or b == 0 or c == 0: sys.exit('Numbers should not be equal to zero! Try again')
+
+def quadratic(a, b, c):
+    discrim = b ** 2 - 4 * a * c
+    if discrim < 0:
+        print( f'Equation is: {a}x^2 + {b}x + {c} = 0 \nThere are 0 roots' )
+        print('Discriminant value is negative.')
+        return
+    if discrim == 0:
+        x = -b / 2 * a
+        print( f'Equation is: {a}x^2 + {b}x + {c} = 0 \nThere are 1 root \nx === {x}')
+        return
+    if discrim >= 0:
+        x1 = ( -b + math.sqrt(discrim) ) / 2 * a
+        x2 = ( -b - math.sqrt(discrim) ) / 2 * a
+        print( f'Equation is: {a}x^2 + {b}x + {c} = 0 \nThere are 2 roots \nx1 === {x1} \nx2 === {x2}')
+        return
+
+quadratic(a, b, c)

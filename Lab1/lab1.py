bafe5669c320813901118c256ed3bb9263dd8de0
Arsenii <71705996+xpadx1@users.noreply.github.com>
2022-02-09 11:43:58 +0200

    Minor input logic changed

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -1,27 +1,49 @@
-
 import sys
 import math
 
-a = int(input('Number a: '))
-b = int(input('Number b: '))
-c = int(input('Number c: '))
+def main():
+    interactiveChooser = int(input('Which mode you would like to use? \nType 1 for interactive mode \nType 0 for non interactive \nEnter a number: '))
 
-if a == 0 or b == 0 or c == 0: sys.exit('Numbers should not be equal to zero! Try again')
+    if interactiveChooser == 1:
+        print('Interactive mode chosen')
+        interactive()
+    elif interactiveChooser == 0:
+        # nonInteractive()
+        print('Non interactive mode chosen')
+    else:
+        print('Type 1 or 0 please! \n')
+        main()
 
-def quadratic(a, b, c):
-    discrim = b ** 2 - 4 * a * c
-    if discrim < 0:
-        print( f'Equation is: {a}x^2 + {b}x + {c} = 0 \nThere are 0 roots' )
-        print('Discriminant value is negative.')
-        return
-    if discrim == 0:
-        x = -b / 2 * a
-        print( f'Equation is: {a}x^2 + {b}x + {c} = 0 \nThere are 1 root \nx === {x}')
-        return
-    if discrim >= 0:
-        x1 = ( -b + math.sqrt(discrim) ) / 2 * a
-        x2 = ( -b - math.sqrt(discrim) ) / 2 * a
-        print( f'Equation is: {a}x^2 + {b}x + {c} = 0 \nThere are 2 roots \nx1 === {x1} \nx2 === {x2}')
-        return
+def interactive():
+    variables = ['a', 'b', 'c']
+    nums = []
+    k = 0
+    while k < len(variables):
+          temp = 0
+          temp = input(f'Number {variables[k]}: ')
+          if not temp.isdigit(): 
+            print(f'Error. Expected a valid real number, got {temp} instead')
+          else:
+            nums.append(int(temp))
+            k += 1
 
-quadratic(a, b, c)
+    def quadratic(a, b, c):
+        discrim = b ** 2 - 4 * a * c
+        if discrim < 0:
+            print( f'Equation is: {a}x^2 + {b}x + {c} = 0 \nThere are 0 roots' )
+            return
+        if discrim == 0:
+            x = -b / 2 * a
+            print( f'Equation is: {a}x^2 + {b}x + {c} = 0 \nThere are 1 root \nx === {x}')
+            return
+        if discrim >= 0:
+            x1 = ( -b + math.sqrt(discrim) ) / 2 * a
+            x2 = ( -b - math.sqrt(discrim) ) / 2 * a
+            print( f'Equation is: {a}x^2 + {b}x + {c} = 0 \nThere are 2 roots \nx1 === {x1} \nx2 === {x2}')
+            return
+
+    quadratic(nums[0], nums[1], nums[2])
+
+# def nonInteractive():
+
+main()
